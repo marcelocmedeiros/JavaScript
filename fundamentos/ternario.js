2a2808da515f6776a3a61dc100cbed2346431f8f
Marcelo Campos de Medeiros <marcelocm31@gmail.com>
2021-04-21 06:07:48 -0300

    Erro

---
+++ fundamentos/ternario.js
@@ -0,0 +1,6 @@
+//              funç arrow expressão ternario vdd      falso
+
+const resultado =   nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
+
+console.log(resultado(7.1))
+console.log(resultado(6.7))
